d4cb8315c9ee471847dce35018e5917bfc960a1f
vo-hp <vohuuphuc1911@gmail.com>
2024-01-30 22:11:28 +0700

    Update Ngat1.ino

--- Ngat/Ngat1/Ngat1.ino
+++ Ngat/Ngat1/Ngat1.ino
@@ -1,20 +1,50 @@
 int button1 = 3;
 int button2 = 4;
-int interruptPin = 2; 
+int valuebt1;
+int valuebt2;
+int interruptPin = 2;  
+int a = 0;
+int i = 0;
+
+int pwmPin = 12;
+int out1 = 11;
+int out2 = 10;
+
 bool wasHigh1 = false;
 bool wasHigh2 = false;
-int a = 0;
 
 void emergencyBrake() {
   a = 0;
 }
 
-void accleration() {
-  
+void acceleration() {
+  if ( valuebt1 == LOW and !wasHigh1 and valuebt2 == HIGH) {
+    for (; i < 255 ; i++) {
+      a = a + 1;
+      delay(40);
+      Serial.println(a);
+      if ( i >= 255) {
+        break;
+      }      
+    }  
+  }
+}
+
+void deceleration() {
+  if ( valuebt2 == LOW and !wasHigh2 and valuebt1 == HIGH) {
+      for (; i >= 0; i--) {
+      a = a - 1;
+      delay(40);
+      Serial.println(a);
+      if ( a <= 0) {
+        break;
+      }
+    }  
+  }
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   pinMode(button1, INPUT);
   pinMode(button2, INPUT);
   pinMode(interruptPin, INPUT_PULLUP);
@@ -22,15 +52,12 @@ void setup() {
 }
 
 void loop() {
-  int valuebt1 = digitalRead(button1);
-  int valuebt2 = digitalRead(button2);
-  if ( valuebt1 == LOW and !wasHigh1 and valuebt2 == HIGH) {
-    a = a+1;
-  }
-  if ( valuebt2 == LOW and !wasHigh2 and valuebt1 == HIGH) {
-    a = a+1;
-  }
+  valuebt1 = digitalRead(button1);
+  valuebt2 = digitalRead(button2);
+  acceleration();
+  deceleration();
   wasHigh1 = !valuebt1;
   wasHigh2 = !valuebt2;
-  Serial.println(a);
+  Serial.println("1:" + String(valuebt1));
+  Serial.println("2:" + String(valuebt2));
 }
