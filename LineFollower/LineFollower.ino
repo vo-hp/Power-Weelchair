10a6e67712189342737c8790db48e2c513be60f3
vo-hp <vohuuphuc1911@gmail.com>
2024-04-21 00:52:45 +0700

    update

--- LineFollower/LineFollower.ino
+++ LineFollower/LineFollower.ino
@@ -1,13 +1,11 @@
-int s1 = 8;
-int s2 = 9;
+int s1 = 12;
+int s2 = 11;
 int s3 = 10;
-int s4 = 11;
-int s5 = 12;
-int out3R;
-int out4R;
-int out1L;
-int out2L;
-int pwmPin;
+int s4 = 9;
+int s5 = 8;
+const int enaA = A2, enaB = A3; // enaA and enaB
+const int out1 = 7, out2 = 6, out3 = 5, out4 = 4; // const int out1 = 14, out2 = 15, out3 = 16, out4 = 17; for ATMega
+int speedMotorA, speedMotorB;
 
 void setup() {
   Serial.begin(9600);
@@ -16,214 +14,214 @@ void setup() {
   pinMode(s3 ,INPUT);
   pinMode(s4 ,INPUT);
   pinMode(s5 ,INPUT);
+  pinMode( out1, OUTPUT);
+  pinMode(out2, OUTPUT);
+  pinMode(out3, OUTPUT);
+  pinMode(out4, OUTPUT);
+  pinMode(enaA, OUTPUT);
+  pinMode(enaB, OUTPUT);
 }
 
 void forward() {
-  digitalWrite(out1L, HIGH);
-  digitalWrite(out2L, LOW);
-  digitalWrite(out3R, HIGH);
-  digitalWrite(out4R, LOW);
+  digitalWrite(out1, HIGH);
+  digitalWrite(out2, LOW);
+  digitalWrite(out3, HIGH);
+  digitalWrite(out4, LOW);
 
 }
 
 void backward() {
-  digitalWrite(out1L, LOW);
-  digitalWrite(out2L, HIGH);
-  digitalWrite(out3R, LOW);
-  digitalWrite(out4R, HIGH);
+  digitalWrite(out1, LOW);
+  digitalWrite(out2, HIGH);
+  digitalWrite(out3, LOW);
+  digitalWrite(out4, HIGH);
 }
 
 void turnRight() {
-  digitalWrite(out1L, HIGH);
-  digitalWrite(out2L, LOW);
-  digitalWrite(out3R, LOW);
-  digitalWrite(out4R, HIGH);
+  digitalWrite(out1, HIGH);
+  digitalWrite(out2, LOW);
+  digitalWrite(out3, LOW);
+  digitalWrite(out4, HIGH);
 }
 
 void turnLeft() {
-  digitalWrite(out1L, LOW);
-  digitalWrite(out2L, HIGH);
-  digitalWrite(out3R, HIGH);
-  digitalWrite(out4R, LOW);
+  digitalWrite(out1, LOW);
+  digitalWrite(out2, HIGH);
+  digitalWrite(out3, HIGH);
+  digitalWrite(out4, LOW);
 }
 
 
 
 void loop() {
-  Serial.println("s1 " + String(digitalRead(s1))); 
-  Serial.println("s2 " +String(digitalRead(s2)));
-  Serial.println("s3 " +String(digitalRead(s3)));
-  Serial.println("s4 " +String(digitalRead(s4)));
-  Serial.println("s5 " +String(digitalRead(s5))); //analogRead(s5)
-  delay(200);
-  if ( s5 == 0 or s4 == 0  ) {
-    turnLeft();
-  }
-  if (s1 == 0 or s2 == 0) {
-    turnRight();
-  }
-  if ( s3 == 0 or s3 == 0 and s2 == 0 and s4 ==0 ) {
+  int v1 = digitalRead(s1);
+  int v2 = digitalRead(s2);
+  int v3 = digitalRead(s3);
+  int v4 = digitalRead(s4);
+  int v5 = digitalRead(s5);
+  Serial.print("s1 " + String(v1)); 
+  Serial.print("   s2 " + String(v2));
+  Serial.print("   s3 " +String(v3));
+  Serial.print("   s4 " +String(v4));
+  Serial.println("   s5 " +String(v5)); //analogRead(s5)
+  middle detects line
+  if ( (( v1 == 1) and ( v2 == 1) and ( v3 == 0 ) and ( v4 == 1) and ( v5 == 1)) or ( v1 == 1  and v2 == 0 and v3 == 0 and v4 == 0 and v5 == 1))  {
     forward();
+    analogWrite(enaA, 70);
+    analogWrite(enaB, 70);
   }
-  if ( s1 == 1 and s2 ==1 and s3 == 1 and s4 == 1 and s5 ==1 ){
-    backward();
+  // far left detect line
+  if (( v1 == 0) and ( v2 == 1) and ( v3 == 1) and ( v4 == 1) and ( v5 == 1)) {
+    turnRight();
+    analogWrite(enaA, 70);
+    analogWrite(enaB, 70);
+  }
+  //  left detects line
+  if (( v1 == 0) and ( v2 == 0) and ( v3 == 1) and ( v4 == 1) and ( v5 == 1)) {
+    turnRight();
+    analogWrite(enaA, 70);
+    analogWrite(enaB, 70);
+  }
+  //far right detects line
+  if (( v1 == 1) and ( v2 == 1) and ( v3 == 1) and ( v4 == 1) and ( v5 == 0)) {
+    turnLeft();
+    analogWrite(enaA, 70);
+    analogWrite(enaB, 70);
+  }
+    // right detects line
+  if (( v1 == 1) and ( v2 == 1) and ( v3 == 1) and ( v4 == 0) and ( v5 == 0)) {
+    turnLeft();
+    analogWrite(enaA, 70);
+    analogWrite(enaB, 70);
+  }
+  // no line detection
+  if (( v1 == 1) and ( v2 == 1) and ( v3 == 1) and ( v4 == 1) and ( v5 == 1)) {
+    digitalWrite(out1, LOW);
+    digitalWrite(out2, LOW);
+    digitalWrite(out4, LOW);
+    digitalWrite(out3, LOW);
+    analogWrite(enaA, 0);
+    analogWrite(enaB, 0);
+  }
+
+}
+/*
+  //if only middle sensor detects black line
+  if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 1) && (s5 == 1))
+  {
+    //going forward with full speed 
+    analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
+    analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
+    digitalWrite(m1, HIGH);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, HIGH);
+    digitalWrite(m4, LOW);
+  }
+  
+  //if only left sensor detects black line
+  if((s1 == 1) && (s2 == 0) && (s3 == 1) && (s4 == 1) && (s5 == 1))
+  {
+    //going right with full speed 
+    analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
+    analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
+    digitalWrite(m1, HIGH);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, LOW);
+    digitalWrite(m4, LOW);
+  }
+  
+  //if only left most sensor detects black line
+  if((s1 == 0) && (s2 == 1) && (s3 == 1) && (s4 == 1) && (s5 == 1))
+  {
+    //going right with full speed 
+    analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
+    analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
+    digitalWrite(m1, HIGH);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, LOW);
+    digitalWrite(m4, HIGH);
+  }
+
+  //if only right sensor detects black line
+  if((s1 == 1) && (s2 == 1) && (s3 == 1) && (s4 == 0) && (s5 == 1))
+  {  
+    //going left with full speed 
+    analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
+    analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
+    digitalWrite(m1, LOW);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, HIGH);
+    digitalWrite(m4, LOW);
+  }
+
+  //if only right most sensor detects black line
+  if((s1 == 1) && (s2 == 1) && (s3 == 1) && (s4 == 1) && (s5 == 0))
+  {
+    //going left with full speed 
+    analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
+    analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
+    digitalWrite(m1, LOW);
+    digitalWrite(m2, HIGH);
+    digitalWrite(m3, HIGH);
+    digitalWrite(m4, LOW);
+  }
+
+  //if middle and right sensor detects black line
+  if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 0) && (s5 == 1))
+  {
+    //going left with full speed 
+    analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
+    analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
+    digitalWrite(m1, LOW);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, HIGH);
+    digitalWrite(m4, LOW);
+  }
+
+  //if middle and left sensor detects black line
+  if((s1 == 1) && (s2 == 0) && (s3 == 0) && (s4 == 1) && (s5 == 1))
+  {
+    //going right with full speed 
+    analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
+    analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
+    digitalWrite(m1, HIGH);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, LOW);
+    digitalWrite(m4, LOW);
+  }
+
+  //if middle, left and left most sensor detects black line
+  if((s1 == 0) && (s2 == 0) && (s3 == 0) && (s4 == 1) && (s5 == 1))
+  {
+    //going right with full speed 
+    analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
+    analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
+    digitalWrite(m1, HIGH);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, LOW);
+    digitalWrite(m4, LOW);
+  }
+
+  //if middle, right and right most sensor detects black line
+  if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 0) && (s5 == 0))
+  {
+    //going left with full speed 
+    analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
+    analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
+    digitalWrite(m1, LOW);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, HIGH);
+    digitalWrite(m4, LOW);
+  }
+
+  //if all sensors are on a black line
+  if((s1 == 0) && (s2 == 0) && (s3 == 0) && (s4 == 0) && (s5 == 0))
+  {
+    //stop
+    digitalWrite(m1, LOW);
+    digitalWrite(m2, LOW);
+    digitalWrite(m3, LOW);
+    digitalWrite(m4, LOW);
   }
 }
-
-// Code cho hệ thống điều khiển xe tự hành theo line
-// #define m1 4  //Right Motor MA1
-// #define m2 5  //Right Motor MA2
-// #define m3 2  //Left Motor MB1
-// #define m4 3  //Left Motor MB2
-// #define e1 9  //Right Motor Enable Pin EA
-// #define e2 10 //Left Motor Enable Pin EB
-
-// //**********5 Channel IR Sensor Connection**********//
-// #define ir1 A0
-// #define ir2 A1
-// #define ir3 A2
-// #define ir4 A3
-// #define ir5 A4
-// //*************************************************//
-
-// void setup() {
-//   pinMode(m1, OUTPUT);
-//   pinMode(m2, OUTPUT);
-//   pinMode(m3, OUTPUT);
-//   pinMode(m4, OUTPUT);
-//   pinMode(e1, OUTPUT);
-//   pinMode(e2, OUTPUT);
-//   pinMode(ir1, INPUT);
-//   pinMode(ir2, INPUT);
-//   pinMode(ir3, INPUT);
-//   pinMode(ir4, INPUT);
-//   pinMode(ir5, INPUT);
-// }
-
-// void loop() {
-//   //Reading Sensor Values
-//   int s1 = digitalRead(ir1);  //Left Most Sensor
-//   int s2 = digitalRead(ir2);  //Left Sensor
-//   int s3 = digitalRead(ir3);  //Middle Sensor
-//   int s4 = digitalRead(ir4);  //Right Sensor
-//   int s5 = digitalRead(ir5);  //Right Most Sensor
-
-//   //if only middle sensor detects black line
-//   if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 1) && (s5 == 1))
-//   {
-//     //going forward with full speed 
-//     analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
-//     analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
-//     digitalWrite(m1, HIGH);
-//     digitalWrite(m2, LOW);
-//     digitalWrite(m3, HIGH);
-//     digitalWrite(m4, LOW);
-//   }
-  
-//   //if only left sensor detects black line
-//   if((s1 == 1) && (s2 == 0) && (s3 == 1) && (s4 == 1) && (s5 == 1))
-//   {
-//     //going right with full speed 
-//     analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
-//     analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
-//     digitalWrite(m1, HIGH);
-//     digitalWrite(m2, LOW);
-//     digitalWrite(m3, LOW);
-//     digitalWrite(m4, LOW);
-//   }
-  
-//   //if only left most sensor detects black line
-//   if((s1 == 0) && (s2 == 1) && (s3 == 1) && (s4 == 1) && (s5 == 1))
-//   {
-//     //going right with full speed 
-//     analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
-//     analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
-//     digitalWrite(m1, HIGH);
-//     digitalWrite(m2, LOW);
-//     digitalWrite(m3, LOW);
-//     digitalWrite(m4, HIGH);
-//   }
-
-//   //if only right sensor detects black line
-//   if((s1 == 1) && (s2 == 1) && (s3 == 1) && (s4 == 0) && (s5 == 1))
-//   {
-//     //going left with full speed 
-//     analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
-//     analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
-//     digitalWrite(m1, LOW);
-//     digitalWrite(m2, LOW);
-//     digitalWrite(m3, HIGH);
-//     digitalWrite(m4, LOW);
-//   }
-
-//   //if only right most sensor detects black line
-//   if((s1 == 1) && (s2 == 1) && (s3 == 1) && (s4 == 1) && (s5 == 0))
-//   {
-//     //going left with full speed 
-//     analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
-//     analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
-//     digitalWrite(m1, LOW);
-//     digitalWrite(m2, HIGH);
-//     digitalWrite(m3, HIGH);
-//     digitalWrite(m4, LOW);
-//   }
-
-//   //if middle and right sensor detects black line
-//   if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 0) && (s5 == 1))
-//   {
-//     //going left with full speed 
-//     analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
-//     analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
-//     digitalWrite(m1, LOW);
-//     digitalWrite(m2, LOW);
-//     digitalWrite(m3, HIGH);
-//     digitalWrite(m4, LOW);
-//   }
-
-//   //if middle and left sensor detects black line
-//   if((s1 == 1) && (s2 == 0) && (s3 == 0) && (s4 == 1) && (s5 == 1))
-//   {
-//     //going right with full speed 
-//     analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
-//     analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
-//     digitalWrite(m1, HIGH);
-//     digitalWrite(m2, LOW);
-//     digitalWrite(m3, LOW);
-//     digitalWrite(m4, LOW);
-//   }
-
-//   //if middle, left and left most sensor detects black line
-//   if((s1 == 0) && (s2 == 0) && (s3 == 0) && (s4 == 1) && (s5 == 1))
-//   {
-//     //going right with full speed 
-//     analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
-//     analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
-//     digitalWrite(m1, HIGH);
-//     digitalWrite(m2, LOW);
-//     digitalWrite(m3, LOW);
-//     digitalWrite(m4, LOW);
-//   }
-
-//   //if middle, right and right most sensor detects black line
-//   if((s1 == 1) && (s2 == 1) && (s3 == 0) && (s4 == 0) && (s5 == 0))
-//   {
-//     //going left with full speed 
-//     analogWrite(e1, 255); //you can adjust the speed of the motors from 0-255
-//     analogWrite(e2, 255); //you can adjust the speed of the motors from 0-255
-//     digitalWrite(m1, LOW);
-//     digitalWrite(m2, LOW);
-//     digitalWrite(m3, HIGH);
-//     digitalWrite(m4, LOW);
-//   }
-
-//   //if all sensors are on a black line
-//   if((s1 == 0) && (s2 == 0) && (s3 == 0) && (s4 == 0) && (s5 == 0))
-//   {
-//     //stop
-//     digitalWrite(m1, LOW);
-//     digitalWrite(m2, LOW);
-//     digitalWrite(m3, LOW);
-//     digitalWrite(m4, LOW);
-//   }
-// }
-
+*/
